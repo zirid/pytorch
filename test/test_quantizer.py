b414c74b734bebf160b974afb8eec7abb3f9f0ab
Jerry Zhang <jerryzh168@gmail.com>
2019-08-16 17:43:15 -0700

    [quant][graphmode] Integration tests for initial quantization graph mode
    Summary:
    att
    
    Test Plan:
    python test/test_quantizer.py
    
    Reviewers:
    pt1quant
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: f5ef924bfed2b850a3567dcd5b2d49f3ed5d52f6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24428

--- test/test_quantizer.py
+++ test/test_quantizer.py
@@ -4,9 +4,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import torch.jit
+from torch.jit import ScriptModule
+from torch._jit_internal import Optional
 import torch.nn as nn
 import torch.nn.functional as F
-from common_utils import TestCase
+from common_utils import TestCase, run_tests
+from torch.quantization import QuantStub, DeQuantStub, \
+    quantize, default_eval_fn, QConfig
 # TODO : Quantizer tests to be integrated with CI once quantizer intf hardened
 
 r"""
@@ -148,7 +152,7 @@ class QuantTemplate:
 
 
 class QuantizerTestCase(TestCase):
-    def test_compare_qparam_eager_script_default(self):
+    def test_compare_qparam_eager_script_default_deprecated(self):
         # Simple test case with conv->relu->maxpool
         class TestScriptM(torch.jit.ScriptModule):
             def __init__(self, init_weight=None):
@@ -222,3 +226,120 @@ class QuantizerTestCase(TestCase):
         self.assertTrue('z' in eagerDict and 'z.1' in activationDict)
         self.assertAlmostEqual(eagerDict["z"][0], activationDict["z.1"][0], places=15)
         self.assertAlmostEqual(eagerDict["z"][1], activationDict["z.1"][1], places=15)
+
+    def test_compare_qparam_eager_script_default(self):
+        class Observer(torch.nn.Module):
+            __annotations__ = {'scale' : Optional[torch.Tensor], 'zero_point': Optional[torch.Tensor]}
+            def __init__(self):
+                super(Observer, self).__init__()
+                self.dtype = torch.quint8
+                self.qscheme = torch.per_tensor_affine
+                self.scale, self.zero_point = None, None
+
+            def forward(self, x):
+                print('runing observer')
+                self.scale = torch.tensor([2.0])
+                self.zero_point = torch.tensor([3])
+                return x
+
+            @torch.jit.export
+            def calculate_qparams(self):
+                return self.scale, self.zero_point
+
+        class WeightObserver(Observer):
+            def __init__(self):
+                super(WeightObserver, self).__init__()
+                self.dtype = torch.qint8
+
+        class TestM(nn.Module):
+            def __init__(self, qconfig):
+                super(TestM, self).__init__()
+                self.conv = nn.Conv2d(1, 3, 3).float()
+                self.conv.bias.data.fill_(0.01)
+                self.qconfig = qconfig
+                self.quant = QuantStub()
+                self.dequant = DeQuantStub()
+
+            def forward(self, x):
+                return self.dequant(self.conv(self.quant(x)))
+
+        class TestScriptM(torch.jit.ScriptModule):
+            def __init__(self, init_weight=None):
+                super(TestScriptM, self).__init__()
+                self.conv = nn.Conv2d(1, 3, 3).float()
+                self.conv.bias.data.fill_(0.01)
+
+            @torch.jit.script_method
+            def forward(self, x):
+                y = self.conv(x)
+                return y
+
+        # Test Data
+        data = [(torch.ones(1, 1, 3, 3, dtype=torch.float), 1)]
+
+        # Eager mode
+        fake_qconfig = QConfig(activation=Observer, weight=WeightObserver)
+        eager_module = TestM(fake_qconfig)
+        script_module = TestScriptM()
+        script_module.conv.weight = torch.nn.Parameter(eager_module.conv.weight.detach())
+        # TODO: there is some problem with caching
+        # print('original result: ', script_module(data[0][0]))
+        quantized_eager_module = quantize(eager_module, default_eval_fn, data)
+        print('eager mode:', quantized_eager_module)
+        e = torch.jit.script(quantized_eager_module)
+        torch._C._jit_pass_constant_propagation(e.graph)
+        print(e.graph)
+        print(e.code)
+
+        def forward_method(m):
+            return m._c._get_method('forward')
+        # Script mode
+        # TODO: test jit.script as well
+
+        torch._C._jit_pass_constant_propagation(script_module.graph)
+
+        ScriptedObserver = torch.jit.script(Observer())
+        ScriptedWeightObserver = torch.jit.script(WeightObserver())
+        print('--------- 1. Prepare Quant -------------')
+        # This is the planed API
+        # torch._C._jit_pass_prepare_quant(script_module._c, "forward", {x:y._c for x,y in qconfig}, backend)
+        print('before preapre:', script_module.graph)
+        script_module._c = torch._C._jit_pass_prepare_quant(script_module._c, "forward", ScriptedObserver._c, ScriptedWeightObserver._c)
+        print('after observer:', script_module.graph)
+
+        # Run ScriptM Model and Collect statistics
+        print('--------- 2. Calibration -------------')
+        forward_method(script_module)(data[0][0])
+
+        # Insert quantize and dequantize calls
+        print('--------- 3. Convert -------------')
+        print('before convert:', forward_method(script_module).graph)
+        print(script_module._c._get_modules())
+        script_module._c = torch._C._jit_pass_insert_quant_dequant(script_module._c, "forward")
+        # res = script_module(data[0][0])
+        print(forward_method(script_module).graph)
+        print(forward_method(script_module).code)
+        print('before fusion')
+        # torch._C._jit_pass_custom_pattern_based_rewrite_graph()
+        print('--------- 4. Fusion -------------')
+        torch._C._jit_pass_quant_fusion(script_module._c._get_method('forward').graph)
+        print('after fusion:')
+        print(forward_method(script_module).graph)
+        print(forward_method(script_module).code)
+        eager_result = quantized_eager_module(data[0][0])
+        script_result = forward_method(script_module)(data[0][0])
+        print(eager_result)
+        print(script_result)
+
+
+        # Compare results for eager and graph mode
+        # eagerDict = eagerQuantObj.getQParamDict()
+        # activationDict = activationQuantObj.getQParamDict()
+
+        # # TODO - fix @eellison
+        # self.assertTrue('z' in eagerDict and 'z.1' in activationDict)
+        # self.assertAlmostEqual(eagerDict["z"][0], activationDict["z.1"][0], places=15)
+        # self.assertAlmostEqual(eagerDict["z"][1], activationDict["z.1"][1], places=15)
+
+if __name__ == '__main__':
+    run_tests()
