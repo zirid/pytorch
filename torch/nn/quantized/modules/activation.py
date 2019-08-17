dcaa9720519772ab382443896bf0a9d678b98338
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-08-16 17:32:36 -0700

    Added relu6 kernel
    ghstack-source-id: de5f0c0dfe1ffbc43f6d34370b52c1842eaf7926
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/24799

--- torch/nn/quantized/modules/activation.py
+++ torch/nn/quantized/modules/activation.py
@@ -3,8 +3,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .. import functional as F
+import torch
 from torch.nn import ReLU as NNReLU
+
+from .. import functional as F
 
 class ReLU(NNReLU):
     r"""Applies quantized rectified linear unit function element-wise:
@@ -35,6 +37,41 @@ class ReLU(NNReLU):
 
     def forward(self, input):
         return F.relu(input)
+
+    @staticmethod
+    def from_float(mod):
+        return ReLU(mod.inplace)
+
+
+class ReLU6(NNReLU):
+    r"""Applies the element-wise function:
+
+    :math:`\text{ReLU6}(x) = \min(\max(x_0, x), q(6))`, where :math:`x_0` is the
+    zero_point, and :math:`q(6)` is the quantized representation of number 6.
+
+    Args:
+        inplace: can optionally do the operation in-place. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, *)` where `*` means, any number of additional
+          dimensions
+        - Output: :math:`(N, *)`, same shape as the input
+
+    .. image:: scripts/activation_images/ReLU6.png
+
+    Examples::
+
+        >>> m = nn.quantized.ReLU6()
+        >>> input = torch.randn(2)
+        >>> input = torch.quantize_linear(input, 1.0, 0, dtype=torch.qint32)
+        >>> output = m(input)
+    """
+    def __init__(self, inplace=False):
+        super(ReLU6, self).__init__(inplace)
+        assert not inplace, 'torch.nn.quantized.ReLU does not support inplace'
+
+    def forward(self, input):
+        return torch.ops.quantized.relu6(input)
 
     @staticmethod
     def from_float(mod):
